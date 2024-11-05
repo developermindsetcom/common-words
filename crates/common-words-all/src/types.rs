a3d35ef99517c8379f4378d8eb68b98542233441
Eugene Hauptmann <eugene@reactivelions.com>
2024-11-05 16:56:16 -0500

    adde default implementations

--- crates/common-words-all/src/types.rs
+++ crates/common-words-all/src/types.rs
@@ -27,6 +27,29 @@ pub enum Language {
     Spanish,
 }
 
+impl Default for Language {
+    fn default() -> Self {
+        #[cfg(feature = "chinese")]
+        let lang = Self::Chinese;
+        #[cfg(feature = "english")]
+        let lang = Self::English;
+        #[cfg(feature = "french")]
+        let lang = Self::French;
+        #[cfg(feature = "german")]
+        let lang = Self::German;
+        #[cfg(feature = "hebrew")]
+        let lang = Self::Hebrew;
+        #[cfg(feature = "italian")]
+        let lang = Self::Italian;
+        #[cfg(feature = "russian")]
+        let lang = Self::Russian;
+        #[cfg(feature = "spanish")]
+        let lang = Self::Spanish;
+
+        lang
+    }
+}
+
 pub enum NgramSize {
     #[cfg(feature = "one")]
     One,
@@ -38,4 +61,30 @@ pub enum NgramSize {
     Four,
     #[cfg(feature = "five")]
     Five,
+}
+
+impl Default for NgramSize {
+    fn default() -> Self {
+        #[allow(unused_variables)]
+        #[cfg(feature = "one")]
+        let size = Self::One;
+        
+        #[allow(unused_variables)]
+        #[cfg(feature = "two")]
+        let size = Self::Two;
+        
+        #[allow(unused_variables)]
+        #[cfg(feature = "three")]
+        let size = Self::Three;
+        
+        #[allow(unused_variables)]
+        #[cfg(feature = "four")]
+        let size = Self::Four;
+        
+        #[allow(unused_variables)]
+        #[cfg(feature = "five")]
+        let size = Self::Five;
+
+        size
+    }
 }
